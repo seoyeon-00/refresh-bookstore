e9744f31c1388d01680d3b75343de6db0f79e5b5
ksy <ksoup3434@gmail.com>
2023-07-26 17:39:36 +0900

    [modify] dotenvConfig delete

--- src/main/kotlin/kr/kro/refbook/utils/Dotenv.kt
+++ src/main/kotlin/kr/kro/refbook/utils/Dotenv.kt
@@ -1,25 +1,25 @@
-package kr.kro.refbook.utils
+// package kr.kro.refbook.utils
 
-import io.github.cdimascio.dotenv.Dotenv
+// import io.github.cdimascio.dotenv.Dotenv
 
-object EnvLoader {
-    private val dotenv: Dotenv by lazy {
-        try {
-            Dotenv.configure().directory(".").ignoreIfMissing().load()
-        } catch (e: Exception) {
-            throw IllegalStateException("Failed to load .env file", e)
-        }
-    }
+// object EnvLoader {
+//     private val dotenv: Dotenv by lazy {
+//         try {
+//             Dotenv.configure().directory(".").ignoreIfMissing().load()
+//         } catch (e: Exception) {
+//             throw IllegalStateException("Failed to load .env file", e)
+//         }
+//     }
 
-    fun getProperty(key: String): String? {
-        return dotenv[key]
-    }
+//     fun getProperty(key: String): String? {
+//         return dotenv[key]
+//     }
 
-    fun getPropertyOrDefault(key: String, defaultValue: String): String {
-        return dotenv[key] ?: defaultValue
-    }
+//     fun getPropertyOrDefault(key: String, defaultValue: String): String {
+//         return dotenv[key] ?: defaultValue
+//     }
 
-    fun getAllProperties(): Map<String, String> {
-        return dotenv.entries().associateBy({ it.key }, { it.value })
-    }
-}
+//     fun getAllProperties(): Map<String, String> {
+//         return dotenv.entries().associateBy({ it.key }, { it.value })
+//     }
+// }
