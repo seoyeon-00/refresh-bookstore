7456b5504cee83dbe72f40d51ec5d6d3ae78225b
ksy <ksoup3434@gmail.com>
2023-07-26 22:59:42 +0900

    [modify] cors error modify

--- src/main/kotlin/kr/kro/refbook/common/authority/SecurityConfig.kt
+++ src/main/kotlin/kr/kro/refbook/common/authority/SecurityConfig.kt
@@ -8,9 +8,11 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.context.annotation.Import
 
 @Configuration
 @EnableWebSecurity
+@Import(CorsConfig::class)
 class SecurityConfig(
     private val jwtTokenProvider: JwtTokenProvider,
 ) {
