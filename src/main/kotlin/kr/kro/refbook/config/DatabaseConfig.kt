91e869978dff4392e17a39f95fccbd914adb7023
HOJUN <bmp.tom@yahoo.com>
2023-08-01 17:36:01 +0900

    [modify] Database Config

--- src/main/kotlin/kr/kro/refbook/config/DatabaseConfig.kt
+++ src/main/kotlin/kr/kro/refbook/config/DatabaseConfig.kt
@@ -1,24 +1,18 @@
 package kr.kro.refbook.config
 
 import io.github.cdimascio.dotenv.Dotenv
-import io.github.cdimascio.dotenv.dotenv
 import org.jetbrains.exposed.sql.Database
 
-val dotenv: Dotenv = dotenv()
-
 object DatabaseConfig {
-    fun init() {
-//          val dbHost: String = dotenv["DB_HOST"] ?: ""
-//          val dbPort: String = dotenv["DB_PORT"] ?: ""
-//          val dbName: String = dotenv["DB_NAME"] ?: ""
-//          val dbUser: String = dotenv["DB_USER"] ?: ""
-//          val dbPassword: String = dotenv["DB_PASSWORD"] ?: ""
+    private val dotenv = Dotenv.load()
 
-        val dbHost: String = System.getenv("DB_HOST") ?: ""
-        val dbPort: String = System.getenv("DB_PORT") ?: ""
-        val dbName: String = System.getenv("DB_NAME") ?: ""
-        val dbUser: String = System.getenv("DB_USER") ?: ""
-        val dbPassword: String = System.getenv("DB_PASSWORD") ?: ""
+    fun init() {
+
+        val dbHost: String = dotenv["DB_HOST"] ?: ""
+        val dbPort: String = dotenv["DB_PORT"] ?: ""
+        val dbName: String = dotenv["DB_NAME"] ?: ""
+        val dbUser: String = dotenv["DB_USER"] ?: ""
+        val dbPassword: String = dotenv["DB_PASSWORD"] ?: ""
 
         Database.connect(
             url = "jdbc:postgresql://$dbHost:$dbPort/$dbName",
