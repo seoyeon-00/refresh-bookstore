d3326ba920970da2308a006789f4750bc55178ba
ksy <ksoup3434@gmail.com>
2023-08-10 20:17:19 +0900

    [modify] dev test

--- src/main/kotlin/kr/kro/refbook/config/RedisConfig.kt
+++ src/main/kotlin/kr/kro/refbook/config/RedisConfig.kt
@@ -1,36 +1,36 @@
-// package kr.kro.refbook.config
+package kr.kro.refbook.config
 
-// import org.springframework.beans.factory.annotation.Value
-// import org.springframework.context.annotation.Bean
-// import org.springframework.context.annotation.Configuration
-// import org.springframework.data.redis.connection.RedisConnectionFactory
-// import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-// import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-// import org.springframework.data.redis.core.RedisTemplate
-// import org.springframework.data.redis.serializer.StringRedisSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
 
-// @Configuration
-// public class RedisConfig {
+@Configuration
+public class RedisConfig {
 
-//     @Value("\${spring.data.redis.host}")
-//     private lateinit var host: String
+    @Value("\${spring.data.redis.host}")
+    private lateinit var host: String
 
-//     @Value("\${spring.data.redis.port}")
-//     private var port: Int = 0
+    @Value("\${spring.data.redis.port}")
+    private var port: Int = 0
 
-//     @Bean
-//     fun redisConnectionFactory(): RedisConnectionFactory {
-//         val redisStandaloneConfiguration = RedisStandaloneConfiguration()
-//         redisStandaloneConfiguration.hostName = host
-//         redisStandaloneConfiguration.port = port
-//         return LettuceConnectionFactory(redisStandaloneConfiguration)
-//     }
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
+        redisStandaloneConfiguration.hostName = host
+        redisStandaloneConfiguration.port = port
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
+    }
 
-//     @Bean
-//     fun redisTemplate(): RedisTemplate<String, Any> {
-//         val redisTemplate = RedisTemplate<String, Any>()
-//         redisTemplate.connectionFactory = redisConnectionFactory()
-//         redisTemplate.keySerializer = StringRedisSerializer()
-//         return redisTemplate
-//     }
-// }
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        val redisTemplate = RedisTemplate<String, Any>()
+        redisTemplate.connectionFactory = redisConnectionFactory()
+        redisTemplate.keySerializer = StringRedisSerializer()
+        return redisTemplate
+    }
+}
