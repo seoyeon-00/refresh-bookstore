d031fcdd91ed99619e6d45907d65bb0eb0812fbf
HOJUN <bmp.tom@yahoo.com>
2023-08-23 16:16:15 +0900

    [fix] Redis

--- src/main/kotlin/kr/kro/refbook/config/RedisConfig.kt
+++ src/main/kotlin/kr/kro/refbook/config/RedisConfig.kt
@@ -4,25 +4,33 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.net.URI
 
 @Configuration
 public class RedisConfig {
 
-    @Value("\${spring.data.redis.host}")
-    private lateinit var host: String
-
-    @Value("\${spring.data.redis.port}")
-    private var port: Int = 0
+    @Value("\${spring.data.redis.url}")
+    private lateinit var redisUrl: String
 
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
+        val uri = URI(redisUrl)
         val redisStandaloneConfiguration = RedisStandaloneConfiguration()
-        redisStandaloneConfiguration.hostName = host
-        redisStandaloneConfiguration.port = port
+
+        redisStandaloneConfiguration.hostName = uri.host
+        redisStandaloneConfiguration.port = uri.port
+
+        // 패스워드가 있을 경우 적용
+        if (uri.userInfo != null) {
+            val password = uri.userInfo.split(":").last()
+            redisStandaloneConfiguration.password = RedisPassword.of(password)
+        }
+
         return LettuceConnectionFactory(redisStandaloneConfiguration)
     }
 
