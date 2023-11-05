ecc0a776118f621637d2837b9766076429307a20
ksy <ksoup3434@gmail.com>
2023-11-06 00:10:40 +0900

    [add] token error response

--- src/main/kotlin/kr/kro/refbook/common/authority/JwtAuthenticationFilter.kt
+++ src/main/kotlin/kr/kro/refbook/common/authority/JwtAuthenticationFilter.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.util.StringUtils
 import org.springframework.web.filter.GenericFilterBean
@@ -18,6 +19,10 @@ class JwtAuthenticationFilter(
         if (token != null && jwtTokenProvider.validateToken(token)) {
             val authentication = jwtTokenProvider.getAuthentication(token)
             SecurityContextHolder.getContext().authentication = authentication
+        } else {
+            // 토큰이 유효하지 않은 경우 403 에러를 반환
+            (response as HttpServletResponse).sendError(HttpServletResponse.SC_FORBIDDEN, "토큰이 유효하지 않습니다.")
+            return
         }
 
         chain?.doFilter(request, response)
