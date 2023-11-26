bb93b22179ff7125587172153959aa13f187870a
ksy <ksoup3434@gmail.com>
2023-11-26 23:27:16 +0900

    [fix] when token is null, setting

--- src/main/kotlin/kr/kro/refbook/common/authority/JwtAuthenticationFilter.kt
+++ src/main/kotlin/kr/kro/refbook/common/authority/JwtAuthenticationFilter.kt
@@ -12,12 +12,31 @@ class JwtAuthenticationFilter(
     private val jwtTokenProvider: JwtTokenProvider,
 ) : GenericFilterBean() {
 
+    // override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+    //     val token = resolveToken(request as HttpServletRequest)
+
+    //     if (token != null && jwtTokenProvider.validateToken(token)) {
+    //         val authentication = jwtTokenProvider.getAuthentication(token)
+    //         SecurityContextHolder.getContext().authentication = authentication
+    //     }
+
+    //     chain?.doFilter(request, response)
+    // }
+
     override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
         val token = resolveToken(request as HttpServletRequest)
 
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            val authentication = jwtTokenProvider.getAuthentication(token)
-            SecurityContextHolder.getContext().authentication = authentication
+        if (token != null) {
+            try {
+                // 토큰이 유효하면 Authentication 객체를 설정
+                if (jwtTokenProvider.validateToken(token)) {
+                    val authentication = jwtTokenProvider.getAuthentication(token)
+                    SecurityContextHolder.getContext().authentication = authentication
+                }
+            } catch (e: Exception) {
+                // 토큰이 유효하지 않아도 그냥 통과
+                // 여기서 예외를 무시하도록 수정
+            }
         }
 
         chain?.doFilter(request, response)
