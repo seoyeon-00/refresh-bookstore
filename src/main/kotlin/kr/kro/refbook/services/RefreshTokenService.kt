10cdb8979ce1ef3e523759c783d30947206a2e82
ksy <ksoup3434@gmail.com>
2023-11-28 00:37:38 +0900

    [fix] refresh token available without access token

--- src/main/kotlin/kr/kro/refbook/services/RefreshTokenService.kt
+++ src/main/kotlin/kr/kro/refbook/services/RefreshTokenService.kt
@@ -1,5 +1,8 @@
 package kr.kro.refbook.services
 
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.redis.core.RedisTemplate
@@ -17,12 +20,27 @@ class RefreshTokenService {
     @Value("\${refresh.token.expiration}")
     private lateinit var refreshTokenExpiration: String
 
+    private val secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256)
+
     fun generateRefreshToken(oldRefreshToken: String? = null): Pair<String, Long> {
         oldRefreshToken?.let { deleteRefreshToken(it) }
 
-        val refreshToken = UUID.randomUUID().toString()
+        //val refreshToken = UUID.randomUUID().toString()
+        val refreshToken = createJwtToken()
         saveRefreshToken(refreshToken)
         return Pair(refreshToken, refreshTokenExpiration.toLong())
+    }
+
+    private fun createJwtToken(): String {
+        val now = Date()
+        val expiration = Date(now.time + refreshTokenExpiration.toLong() * 1000) // seconds to milliseconds
+
+        return Jwts.builder()
+            .setSubject("refreshToken")
+            .setIssuedAt(now)
+            .setExpiration(expiration)
+            .signWith(secretKey, SignatureAlgorithm.HS256)
+            .compact()
     }
 
     private fun saveRefreshToken(refreshToken: String) {
